48d7af8eae35f5db234b7deb51073b47d8b9f174
danedehaan <danedehaan@qubitsec.com>
2019-09-29 22:35:13 +0900

    리액트 함수형 컴포넌트 에서 clearInterval   처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,64 @@
-import React,{useState,useCallback} from 'react';
+import React,{useState,useCallback,useRef} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
+
+  let intervalId:any = useRef(null)
+
   const [timer , setTimer] = useState(30)
   const [disabled , setDisabled] = useState(false)
-  const [intervalId , setIntervalId]:[any,Function] = useState(null) 
-
-  const startTimer = useCallback(()=>{
-    setDisabled(true)
-    let time = timer
-    const current = setInterval(()=>{
-      console.log("timer",time)
-      if(time > 0){
-        setTimer(--time)
-      }else{
-        console.log("clearInterval",intervalId)
-        clear()
-        alert("알람끝")
-      }
-    },100)
-
-    setIntervalId(current)
-  },[timer])
-
-
-  const stopTimer = ()=>{
-    clear()
-  }
 
   const clear = useCallback(()=>{
     setDisabled(false)
-    clearInterval(intervalId)
-  },[intervalId])
-  // const stopTimer = ()=>{
+    clearInterval(intervalId.current)
+  },[intervalId.current])
 
-  // }
+  const startTimer = useCallback(()=>{
+    draw()
+    setDisabled(true)
+    let time = timer
+    intervalId.current = setInterval(()=>{
+      if(time > 0){
+        setTimer(--time)
+      }else{
+        clear()
+        alert("타이머 끝")
+      }
+    },100)
+  },[intervalId.current,timer])
+
+  const stopTimer = clear
+
+  function draw() {
+    var canvas : any =  document.getElementById('canvas');
+    console.log("canvas",canvas)
+    console.log("canvas",canvas.getContext)
+    if (canvas.getContext) {
+      console.log("canvas")
+      var ctx = canvas.getContext('2d');
+  
+      for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 3; j++) {
+          ctx.beginPath();
+          var x = 25 + j * 50; // x coordinate
+          var y = 25 + i * 50; // y coordinate
+          var radius = 20; // Arc radius
+          var startAngle = 0; // Starting point on circle
+          var endAngle = Math.PI + (Math.PI * j) / 2; // End point on circle
+          var anticlockwise = i % 2 == 0 ? false : true; // clockwise or anticlockwise
+  
+          ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
+  
+          if (i > 1) {
+            ctx.fill();
+          } else {
+            ctx.stroke();
+          }
+        }
+      }
+    }
+  }
 
   return (
     <div className="App">
@@ -47,6 +70,7 @@ const App: React.FC = () => {
         <button onClick={stopTimer}>
           취소하기
         </button>
+        <div id="canvas"/>
         <div>
           {/* {timer} */}
           <input type="text" onChange={(e)=>{setTimer(Number(e.target.value))}} value={timer} disabled={disabled}/>
