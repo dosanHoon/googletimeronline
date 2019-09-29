6991e0aafa85b7d53d82099258f6732c0decffd4
danedehaan <danedehaan@qubitsec.com>
2019-09-29 22:01:38 +0900

    시간 입력 기능 시간 타이머 스타트 버튼 기능 시간 타이머 완료 시 알러트 팝업 기능

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import React from 'react';
+import React,{useState,useCallback} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
+  const [timer , setTimer] = useState(30)
+
+  const startTimer = useCallback(()=>{
+    let time = timer
+    const current = setInterval(()=>{
+      console.log("timer",time)
+      if(time > 0){
+        setTimer(--time)
+      }else{
+        console.log("clearInterval",current)
+        clearInterval(current)
+        alert("알람끝")
+      }
+    },100)
+  },[timer])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={startTimer}>
+          시작하기
+        </button>
+        <div>
+          {/* {timer} */}
+          <input type="text" onChange={(e)=>{setTimer(Number(e.target.value))}} value={timer}/>
+        </div>
       </header>
     </div>
   );
