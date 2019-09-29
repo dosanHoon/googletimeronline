e3965019b9f6d558e7be806107ecf75638bbe120
danedehaan <danedehaan@qubitsec.com>
2019-09-29 22:09:44 +0900

    정지 기능 추가 기본 적인 타이머 기능 완료

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,38 @@ import './App.css';
 
 const App: React.FC = () => {
   const [timer , setTimer] = useState(30)
+  const [disabled , setDisabled] = useState(false)
+  const [intervalId , setIntervalId]:[any,Function] = useState(null) 
 
   const startTimer = useCallback(()=>{
+    setDisabled(true)
     let time = timer
     const current = setInterval(()=>{
       console.log("timer",time)
       if(time > 0){
         setTimer(--time)
       }else{
-        console.log("clearInterval",current)
-        clearInterval(current)
+        console.log("clearInterval",intervalId)
+        clear()
         alert("알람끝")
       }
     },100)
+
+    setIntervalId(current)
   },[timer])
+
+
+  const stopTimer = ()=>{
+    clear()
+  }
+
+  const clear = useCallback(()=>{
+    setDisabled(false)
+    clearInterval(intervalId)
+  },[intervalId])
+  // const stopTimer = ()=>{
+
+  // }
 
   return (
     <div className="App">
@@ -26,9 +44,12 @@ const App: React.FC = () => {
         <button onClick={startTimer}>
           시작하기
         </button>
+        <button onClick={stopTimer}>
+          취소하기
+        </button>
         <div>
           {/* {timer} */}
-          <input type="text" onChange={(e)=>{setTimer(Number(e.target.value))}} value={timer}/>
+          <input type="text" onChange={(e)=>{setTimer(Number(e.target.value))}} value={timer} disabled={disabled}/>
         </div>
       </header>
     </div>
