84de32626632241d53c4faddd82e1c3630d99058
danedehaan <danedehaan@qubitsec.com>
2019-10-15 15:16:26 +0900

    드래그로 원의 크기 변경
    캔버스 크기 props 받도록
    
    draw 하는 function 따로 만들기 성공

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,50 @@
-import React,{useState,useCallback,useRef} from 'react';
-import './App.css';
-import Circle from './components/Circle';
+import React, { useState, useCallback, useRef } from "react"
+import "./App.css"
+import Circle from "./components/Circle"
 
 const App: React.FC = () => {
+  let intervalId: any = useRef(null)
 
-  let intervalId:any = useRef(null)
+  const [timer, setTimer]: [number, Function] = useState(30)
+  const [disabled, setDisabled] = useState(false)
 
-  const [timer , setTimer] = useState(30)
-  const [disabled , setDisabled] = useState(false)
-
-  const clear = useCallback(()=>{
+  const clear = useCallback(() => {
     setDisabled(false)
     clearInterval(intervalId.current)
-  },[intervalId.current])
+  }, [intervalId.current])
 
-  const startTimer = useCallback(()=>{
-
+  const startTimer = useCallback(() => {
     setDisabled(true)
     let time = timer
-    intervalId.current = setInterval(()=>{
-      if(time > 0){
+    intervalId.current = setInterval(() => {
+      if (time > 0) {
         setTimer(--time)
-      }else{
+      } else {
         clear()
         alert("타이머 끝")
       }
-    },100)
-  },[intervalId.current,timer])
-
-  
+    }, 100)
+  }, [intervalId.current, timer])
 
   return (
     <div className="App">
       <header className="App-header">
-        <Circle timer={timer}/>
+        <Circle timer={timer} width={500} height={500} />
         <div>
-          <input type="text" onChange={(e)=>{setTimer(Number(e.target.value))}} value={timer} disabled={disabled}/>
+          <input
+            type="text"
+            onChange={e => {
+              setTimer(Number(e.target.value))
+            }}
+            value={timer}
+            disabled={disabled}
+          />
         </div>
-        <button onClick={startTimer}>
-          시작하기
-        </button>
-        <button onClick={clear}>
-          취소하기
-        </button>
+        <button onClick={startTimer}>시작하기</button>
+        <button onClick={clear}>취소하기</button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,37 +1,111 @@
-import React,{createRef,useEffect} from 'react'
+import React, { createRef, useEffect, useState, useMemo } from "react"
 
-const Circle = ({timer } :any) => {
-    let canvasRef:any = createRef()
+interface propsTypes {
+  timer: number
+  width: number
+  height: number
+}
 
-    useEffect(()=>{
-        console.log("timer",timer)
-        var canvas : any =  canvasRef.current
-        if (canvas.getContext) {
-            console.log("timer",(Math.PI*2)*(timer/60))
-            var ctx = canvas.getContext('2d');
-            ctx.clearRect(0, 0, 500, 500);
+const Circle = ({ timer, width, height }: propsTypes) => {
+  let canvasRef: any = createRef()
 
-            ctx.beginPath();
-            ctx.moveTo(250, 250);
-            ctx.arc(250,250,250,0, (Math.PI*2), true);
-            ctx.closePath();
-            ctx.stroke();
-            ctx.fillStyle="rgb(255,255,255)"  
-            ctx.fill();  
+  const [dragok, setDragok] = useState(false)
 
-            ctx.beginPath();
-            ctx.moveTo(250, 250);
-            ctx.arc(250,250,250,-0.5 * Math.PI, -0.5 *Math.PI + (Math.PI)*2*(timer/60), false);
-            ctx.closePath();
-            ctx.stroke();
-            ctx.fillStyle="rgb(255,0,0)"  
-            ctx.fill();  
-        }
-    },[timer])
+  const [circleX, circleY] = useMemo(() => [width / 2, height / 2], [width, height])
 
-    return (
-        <canvas  width="500" height="500" ref={canvasRef}/>
-    )
+  const draw = (rad: number) => {
+    const canvas: any = canvasRef.current
+    if (canvas.getContext) {
+      const ctx = canvas.getContext("2d")
+      ctx.clearRect(0, 0, width, height)
+
+      ctx.beginPath()
+      ctx.moveTo(circleX, circleY)
+      ctx.arc(circleX, circleY, circleX, 0, Math.PI * 2, true)
+      ctx.closePath()
+      ctx.stroke()
+      ctx.fillStyle = "rgb(255,255,255)"
+      ctx.fill()
+
+      ctx.beginPath()
+      ctx.moveTo(circleX, circleY)
+      ctx.arc(circleX, circleY, circleX, -0.5 * Math.PI, rad, false)
+      ctx.closePath()
+      ctx.stroke()
+      ctx.fillStyle = "#b13c3c"
+      ctx.fill()
+
+      drawNumbers(ctx, circleX)
+    }
+  }
+
+  useEffect(() => {
+    draw(-0.5 * Math.PI + Math.PI * 2 * (timer / 60))
+  }, [timer])
+
+  // handle mousedown events
+  function myDown(e: any) {
+    e.preventDefault()
+    e.stopPropagation()
+
+    setDragok(true)
+  }
+
+  function myUp(e: any) {
+    e.preventDefault()
+    e.stopPropagation()
+    setDragok(false)
+  }
+
+  function myMove(e: {
+    clientX: any
+    clientY: any
+    preventDefault: Function
+    stopPropagation: Function
+  }) {
+    if (dragok) {
+      e.preventDefault()
+      e.stopPropagation()
+      const canvas: any = canvasRef.current
+      const BB = canvas.getBoundingClientRect()
+      const offsetX: any = BB.left
+      const offsetY: any = BB.top
+      const mx = parseInt((e.clientX - offsetX).toString())
+      const my = parseInt((e.clientY - offsetY).toString())
+      const rads = Math.atan2(my - 250, mx - 250)
+      draw(rads)
+    }
+  }
+
+  function drawNumbers(ctx: any, radius: number) {
+    var ang
+    var num
+    ctx.font = radius * 0.15 + "px arial"
+    ctx.textBaseline = "middle"
+    ctx.textAlign = "center"
+    ctx.translate(radius, radius)
+    for (num = 1; num < 13; num++) {
+      ang = (num * Math.PI) / 6
+      ctx.rotate(ang)
+      ctx.translate(0, -radius * 0.85)
+      ctx.rotate(-ang)
+      ctx.fillText(num.toString(), 0, 0)
+      ctx.rotate(ang)
+      ctx.translate(0, radius * 0.85)
+      ctx.rotate(-ang)
+    }
+  }
+
+  return (
+    <canvas
+      width={width}
+      height={height}
+      ref={canvasRef}
+      onMouseUp={myUp}
+      onMouseDown={myDown}
+      onMouseMove={myMove}
+    />
+  )
 }
 
 export default Circle
